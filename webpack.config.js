05df8aea8faf9cf6b019f3345e35c39723d3d9ea
Pegunito <artyomsasha@mail.ru>
2020-08-10 18:51:55 +0300

    внес изменения в footer

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = (env, argv) => {
   const isProductionBuild = argv.mode === "production";
-  const publicPath ="Portfolio/dist/";
+  const publicPath ="/Portfolio/dist/";
 
   const pcss = {
     test: /\.(p|post|)css$/,

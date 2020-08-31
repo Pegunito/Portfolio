eed9215db90fafb1b48c1e40f8126103da4a7d35
Pegunito <artyomsasha@mail.ru>
2020-08-31 18:33:45 +0300

    добавил компомненты в админ(nav,head.user)

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -1,3 +1,31 @@
-<template lang="pug">
-  h1 Welcome to the Vue App
+<template>
+  <div class="app-container">
+    <headline title="Панель администрирования">
+      <user />
+    </headline>
+    <navigation />
+  </div>
 </template>
+
+<script>
+  import headline from "./components/headline";
+  import user from "./components/user";
+  import navigation from "./components/navigation";
+
+  export default {
+    components: {
+      headline,
+      user,
+      navigation
+    },
+  };
+</script>
+
+<style lang="postcss">
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800');
+  @import "normalize.css";
+  @import "../styles/mixins.pcss";
+  @import "../styles/layout/base.pcss";
+</style>
+
+<style lang="pcss" scoped src="./app.pcss"></style>

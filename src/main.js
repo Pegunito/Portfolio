b0c0f93eceb347bdd053c79700cffd755ef518b8
Pegunito <artyomsasha@mail.ru>
2020-09-22 14:47:45 +0300

    добавил js для секции about

--- src/main.js
+++ src/main.js
@@ -7,3 +7,16 @@ import "./scripts/reviews";
 import "./scripts/skills";
 import "./scripts/works";
 
+// появление информации
+
+function scrollAppear() {
+  var userAppear = document.querySelector('.about__container');
+  var userPosition = userAppear.getBoundingClientRect().top;
+  var screenPosition = window.innerHeight / 1.3;
+
+  if(userPosition < screenPosition) {
+    userAppear.classList.add('user__appear');
+  }
+}
+
+window.addEventListener('scroll', scrollAppear);
